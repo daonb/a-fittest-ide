fd72189faf2f493300d9d7bac673a22c090130f2
benny daon <bennydaon@gmail.com>
2019-08-05 17:03:54 +0300

    Fixing backups, uninstall & logging

--- afide/__init__.py
+++ afide/__init__.py
@@ -8,6 +8,8 @@ import click
 
 logger = None
 
+BACKUP_FILES_NAME = 'backup_files.txt'
+
 @dataclass
 class Config:
     verbose: bool = False
@@ -16,12 +18,14 @@ class Config:
     logger: object = None
 
 def link_file(target, link_name):
+    """Create a link to a target. Returns the name of the backup file """
     link = os.path.expanduser(link_name)
+    ret = ''
     backup = f'{link}.b4afide'
-
     if os.path.isfile(link) and not os.path.isfile(backup):
         shutil.copy2(link, backup)
-        logger.info(f"    Backed up {link} => {backup}")
+        logger.info(f"Backed up {link} => {backup}")
+        ret = backup
 
     if os.path.isfile(link) or os.path.islink(link):
         os.remove(link)
@@ -32,7 +36,8 @@ def link_file(target, link_name):
         os.makedirs(link_dir)
 
     os.symlink(target, link)
-    logger.info(f"    Linked {link} => {target}")
+    logger.info(f"Linked {link} => {target}")
+    return ret
 
 @click.group()
 @click.option('-v', '--verbose', default=False, is_flag=True, help='Be more verbose')
@@ -45,17 +50,19 @@ def cli(ctx, verbose, debug, path):
 
     logger = logging.getLogger()
     ctx.obj = Config(verbose, debug, os.path.expanduser(path), logger)
+    level = logging.WARN
     if verbose:
-        logger.setLevel(logging.INFO)
+        level = logging.INFO
     if debug:
-        print('setting as debug')
-        logger.setLevel(logging.DEBUG)
-        handler = logging.StreamHandler(sys.stdout)
-        handler.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s:AFIDE:%(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-        logger.debug("Setting logLevel to DEBUG")
+        level = logging.DEBUG
+    logger.info(f"Setting loglevel - {level}")
+    logger.setLevel(level)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(level)
+    formatter = logging.Formatter('%(asctime)s:AFIDE:%(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.debug("Setting logLevel to DEBUG")
 
 @cli.command()
 @click.pass_context
@@ -63,20 +70,25 @@ def install(ctx):
     """Link afide conf files to users'"""
     logger = ctx.obj.logger
     conf_path = os.path.join(ctx.obj.path, 'conf')
+    un_file = open(os.path.join(ctx.obj.path, BACKUP_FILES_NAME), 'a')
     for dirpath, dirnames, filenames in os.walk(conf_path):
         for fname in filenames:
             target = os.path.join(dirpath, fname)
-            logger.debug(f">>> {target}")
             with open(target, 'r') as f:
                 first_line = f.readline()
-                logger.debug(f"    first line: {first_line}")
-                if first_line[1] != '=':
-                    logger.info(f"     Skipping")
-                    continue
-                link_name = first_line[2:-1]
+                logger.debug(f"first line: {first_line}")
+                link_name = first_line[2:-1] if first_line[1] == '=' else None
 
             if link_name:
-                link_file(target, link_name)
+                backup = link_file(target, link_name)
+                if backup:
+                    un_file.write(backup)
+                    un_file.write(os.linesep)
+            else:
+                logger.info(f"Not linking to {target}")
+    un_file.close()
+
+
 
 @cli.command()
 def run():
@@ -92,8 +104,15 @@ def verify():
     pass
 
 @cli.command()
-def uninstall():
-    pass
+@click.pass_context
+def uninstall(ctx):
+    logger = ctx.obj.logger
+    f =  open(os.path.join(ctx.obj.path, BACKUP_FILES_NAME), 'r')
+    for backup in f:
+        backup = backup[:-1]
+        logger.debug(f"Restoring {backup}")
+        shutil.copy2(backup, backup[:-8])
+        os.remove(backup)
 
 if __name__ == '__main__':
     cli()
